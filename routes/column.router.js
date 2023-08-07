b6f3e03d000c6e96a196d8451dbd3c441c6ef92e
choisumin <tnals634@naver.com>
2023-08-07 20:20:36 +0900

    feat: column router column router 구현

---
+++ routes/column.router.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { isLoggedIn } = require("../middlewares");
+
+const router = express.Router();
+
+const ColumnController = require("../controllers/column.controller");
+const columnController = new ColumnController();
+
+/** 컬럼 생성 */
+router.post(
+  "/board/:boardId/column",
+  isLoggedIn,
+  columnController.createColumn
+);
+/** 컬럼 목록 조회 */
+router.get("/board/:boardId/column", isLoggedIn, columnController.getAllColumn);
+/** 컬럼 수정 */
+router.put(
+  "/board/:boardId/column/:columnId",
+  isLoggedIn,
+  columnController.updateColumn
+);
+/** 컬럼 삭제 */
+router.delete(
+  "/board/:boardId/column/:columnId",
+  isLoggedIn,
+  columnController.deleteColumn
+);
+
+module.exports = router;
