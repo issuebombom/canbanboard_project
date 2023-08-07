51063b7515e5efde8ea8fc1b38e3846c516298c0
issuebombom <issuebombom@gmail.com>
2023-08-07 17:55:02 +0900

    chore: minor

---
+++ config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const development = {
+  username: 'root',
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql',
+};
+
+const test = {
+  username: 'root',
+  password: null,
+  database: 'database_test',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+};
+
+const production = {
+  username: 'root',
+  password: null,
+  database: 'database_production',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+};
+
+module.exports = { development, test, production };
