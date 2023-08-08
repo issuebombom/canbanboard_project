9cfeae8d3afe3555f32805ece963fa6e0d768fe4
issuebombom <issuebombom@gmail.com>
2023-08-08 14:44:11 +0900

    fix: board 테이블 admins type 변경

--- models/board.js
+++ models/board.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
       },
       admins: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
       },
       color: {
         type: DataTypes.STRING,
